6a9d16d1a4028ad4dee0b41bbd9d02973c04c037
Sushant <sushantverma9648@gmail.com>
2022-10-14 23:02:04 +0530

    JavaScript Animations

---
+++ Animations/index.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#mytext', {
+    strings : ['JavaScript Animations', 'JavaScript Animations 2nd time'],
+    typeSpeed : 100
+})
+
+// Aos -> Animate on Scroll
+
+AOS.init({
+    duration : 2000
+})
